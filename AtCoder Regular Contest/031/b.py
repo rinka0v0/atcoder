0e5bc7b8c7078b1104c4cf6cb5fa0417fc84fb2f
rinka0v0 <rinka0v0@users.noreply.github.com>
2021-06-13 17:07:26 +0900

    dfs

--- AtCoder Regular Contest/031/b.py
+++ AtCoder Regular Contest/031/b.py
@@ -1,3 +1,48 @@
 import sys 
-sys.setrecursionlimit(10000000)
+import copy
+sys.setrecursionlimit(1000000)
 
+ans = False
+
+dh = [0,1,0,-1]
+dw = [1,0,-1,0]
+
+field = list(input() for i in range(10))
+
+seen = list(list(False for i in range(10)) for j in range(10))
+
+for i in range(10):
+    for j in range(10):
+        if field[i][j] == 'x':
+            seen[i][j] = True
+
+def dfs(h,w,s):
+    s[h][w] = True
+     
+    for i in range(4):
+        nextH = h + dh[i]
+        nextW = w + dw[i]
+        if nextH < 0 or 10 <= nextH or nextW < 0 or 10 <= nextW:
+            continue
+        if s[nextH][nextW]:
+            continue
+        dfs(nextH,nextW,s)
+
+for i in range(10):
+    for j in range(10):
+        seenCopy = copy.deepcopy(seen)
+        seenCopy[i][j] = False
+        dfs(i,j,seenCopy)
+
+        count = True
+        for height in range(10):
+            for width in range(10):
+                if seenCopy[height][width] == False:
+                    count = False
+        if count:
+            ans = True
+
+if ans:
+    print('YES')
+else:
+    print('NO')
