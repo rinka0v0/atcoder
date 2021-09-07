7c801760df031bfb3df87b8f31b35f7b16852246
rinka0v0 <rinka0v0@users.noreply.github.com>
2021-09-07 21:31:36 +0900

    全探索

--- JOI/第7回　予選/d.py
+++ JOI/第7回　予選/d.py
@@ -23,3 +23,21 @@ for i in photo:
         print(vectorx,vectory)
     
     
+
+# 2回目解いた時のコード
+m = int(input())
+signs = [list(map(int,input().split())) for i in range(m)]
+n = int(input())
+specimen = [tuple(map(int,input().split())) for i in range(n)]
+set_specimen = set(specimen)
+nowx = signs[0][0]
+nowy = signs[0][1]
+
+for i in range(n):
+    dx = specimen[i][0] - nowx
+    dy = specimen[i][1] - nowy
+    for j in range(m):
+        if not((signs[j][0] + dx,signs[j][1] + dy) in set_specimen):
+            break
+        if j == m-1:
+            print(dx,dy)
